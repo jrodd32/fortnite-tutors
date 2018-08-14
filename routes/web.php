c1dcb280362d05654b58eb85831ac408c3d3a8b3
Jerrod Long <jlong@doeanderson.com>
2018-08-13 22:29:32 -0400

    [add] Route to save a new mailing list user.

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/mailing-list', 'MailingListController@save')->name('mailing-list');
 
