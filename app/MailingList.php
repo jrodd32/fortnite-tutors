662c9d980befa6a622b9fa6b417ff7ac440c1ab8
Jerrod Long <jlong@doeanderson.com>
2018-08-13 23:17:12 -0400

    [add] Set explict $table value to singular mailing_list.

--- app/MailingList.php
+++ app/MailingList.php
@@ -9,4 +9,8 @@ class MailingList extends Model
     protected $fillable = [
       'name', 'email', 'account_type'
     ];
+
+    // Explicitly set table to singular version
+    // Laravel assumes pural
+    protected $table = 'mailing_list';
 }
